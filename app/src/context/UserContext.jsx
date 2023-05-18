cc75f9defe17609606122a41f526cfb76fd96a7e
Shardul Kurdukar <24shardul@gmail.com>
2023-05-18 23:34:50 +0530

    fixed message ordering

--- app/src/context/UserContext.jsx
+++ app/src/context/UserContext.jsx
@@ -7,11 +7,10 @@ export const useUserContext = () => {
   return useContext(UserContext);
 };
 
-
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [conversations, setConversations] = useState([]);
-  const socket= useSocketContext();
+  const socket = useSocketContext();
 
   useEffect(() => {
     if (socket) {
@@ -21,13 +20,24 @@ export const UserProvider = ({ children }) => {
       });
       socket.on("conversation", (conversations) => {
         console.log(conversations);
-        setConversations(conversations);
-      })
+        let sortedMessages = conversations.map((conversation) => {
+          let sortedMessages = conversation.messages.sort((a, b) => {
+            return new Date(a.createdAt) - new Date(b.createdAt);
+          });
+          return { ...conversation, messages: sortedMessages };
+        });
+        let sortedConversations = sortedMessages.sort((a, b) => {
+          return (
+            new Date(b.messages[b.messages.length - 1].createdAt) -
+            new Date(a.messages[a.messages.length - 1].createdAt)
+          );
+        });
+        setConversations(sortedConversations);
+      });
       socket.on("refetch", () => {
         socket.emit("conversation:get");
         socket.emit("user:get");
-      })
-
+      });
     }
   }, [socket]);
 
