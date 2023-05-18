cc4524db82619120a70465376ae4b7c74871beb9
Shardul Kurdukar <24shardul@gmail.com>
2023-05-18 11:34:26 +0530

    ip change

--- server/models/db.js
+++ server/models/db.js
@@ -1,12 +1,22 @@
 const Sequelize = require("sequelize");
 
 //initialize in memory sqlite database
-const db = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
+const db = new Sequelize("chatapp", "postgres", "admin123", {
+  dialect: "postgres",
+  host: "10.99.128.4",
 });
 
-db.sync({ force: false });
-console.log("All models were synchronized successfully.");
+//check connection
+(async () => {
+  try {
+    console.log(db);
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+    db.sync({ force: false });
+    console.log("All models were synchronized successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
 module.exports = db;
